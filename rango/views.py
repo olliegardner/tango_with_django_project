fb33a519a6942071ec6241c6dfbf494f5ad7e74f
Oliver Gardner <2310049G@student.gla.ac.uk>
2019-01-24 01:55:53 +0000

    rectified some mistakes that I made in the exercises at the end of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -10,5 +10,5 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	context_dict = {'boldmessage': 'a dog will appear on this page'}
+	context_dict = {'boldmessage': 'a cat will appear on this page'}
 	return render(request, 'rango/about.html', context=context_dict)
