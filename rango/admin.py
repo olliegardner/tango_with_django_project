05cf374fd565e8b5925f9893a44cf6a7f77e1628
Oliver Gardner <2310049G@student.gla.ac.uk>
2019-01-25 15:58:53 +0000

    completed chapter 5 of TWD

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
