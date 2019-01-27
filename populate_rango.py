da8189fab99d69672e77fcea964aa08864b9d338
Oliver Gardner <2310049G@student.gla.ac.uk>
2019-01-27 18:47:10 +0000

    fixed bug which prevented most viewed pages from showing

--- populate_rango.py
+++ populate_rango.py
@@ -3,6 +3,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.setti
 
 import django
 django.setup()
+
 from rango.models import Category, Page
 
 def populate():
@@ -49,8 +50,8 @@ def populate():
 
 def add_page(cat, title, url, views=0):
 	p = Page.objects.get_or_create(category=cat, title=title)[0] 
-	p.url=url
-	p.views=views
+	p.url = url
+	p.views = views
 	p.save()
 	return p
 
