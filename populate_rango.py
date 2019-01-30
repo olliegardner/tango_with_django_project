f1e6c89e2366deee65c0437cf66daf235dfcc87d
Oliver Gardner <2310049G@student.gla.ac.uk>
2019-01-30 22:16:12 +0000

    finished chapter 7 of TWD

--- populate_rango.py
+++ populate_rango.py
@@ -7,6 +7,8 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
+
+	'''
 	python_cat = add_cat('Python', 128, 64)
 
 	python_pages = [
@@ -38,11 +40,38 @@ def populate():
 	cats = {"Python": {"pages": python_pages},
             "Django": {"pages": django_pages},
             "Other Frameworks": {"pages": other_pages} }
+    '''
+	python_pages = [
+    	{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
+		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},  ]
+    
+	django_pages = [
+		{"title":"Official Django Tutorial",
+	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
+		{"title":"Django Rocks",
+	        "url":"http://www.djangorocks.com/" , "views": 16},
+		{ "title":"How to Tango with Django",
+	        "url":"http://www.tangowithdjango.com/", "views": 8 } ]
+    
+	other_pages = [
+		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
+    
+	cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+			"Django": {"pages": django_pages, "views": 64, "likes": 32},
+        	"Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
+            "Pascal": {"pages": [], "views": 32, "likes": 16},
+            "Perl": {"pages": [], "views": 32, "likes": 16},
+            "Php": {"pages": [], "views": 32, "likes": 16},
+            "Prolog": {"pages": [], "views": 32, "likes": 16},
+            "Programming": {"pages": [], "views": 32, "likes": 16},
+            }
 
 	for cat, cat_data in cats.items(): 
-   		c = add_cat(cat)
+		c = add_cat(cat, cat_data["views"], cat_data["likes"])
    		for p in cat_data["pages"]:
-			add_page(c, p["title"], p["url"])
+			add_page(c, p["title"], p["url"], p["views"])
 
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
@@ -50,15 +79,15 @@ def populate():
 
 def add_page(cat, title, url, views=0):
 	p = Page.objects.get_or_create(category=cat, title=title)[0] 
-	p.url = url
-	p.views = views
+	p.url=url
+	p.views=views
 	p.save()
 	return p
 
 def add_cat(name, views=0, likes=0):
 	c = Category.objects.get_or_create(name=name)[0]
-	c.views = views
-	c.likes = likes 
+	c.views=views
+	c.likes=likes 
 	c.save()
 	return c
 
--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -27,3 +28,39 @@ def show_category(request, category_name_slug):
 		context_dict['pages'] = None
 
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			category = form.save(commit=True)
+			print(category, category.slug)
+			return index(request)
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+
+	context_dict = {'form': form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
